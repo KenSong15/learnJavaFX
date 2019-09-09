19ae5b456d2354e69f464f6754e175976ea95eeb
Ken <kensong96@gmail.com>
2019-09-09 19:58:49 -0400

    5.alert box

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -24,31 +22,49 @@ public class Main extends Application{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //primaryStage.setTitle("Hello World");
 
-        //1. switch scene===
+//        //1. switch scene===
+//        window = primaryStage;
+//
+//        Label label1 = new Label("this is the first scene.");
+//        Button button1 = new Button("go to scene 2");
+//        button1.setOnAction(e -> window.setScene(scene2));
+//
+//        Label label2 = new Label("this scene bad.");
+//        Button button2 = new Button("go back to scene 1");
+//        button2.setOnAction(e -> window.setScene(scene1));
+//
+//        //layout 1 - children are laid out in vertical column
+//        VBox layout1 = new VBox(20);
+//        layout1.getChildren().addAll(label1,button1);
+//        scene1 = new Scene(layout1, 200,200);
+//
+//        //layout 2 - laid out in horizontal
+//        HBox layout2 = new HBox();
+//        layout2.getChildren().addAll(button2, label2);
+//        scene2 = new Scene(layout2, 600, 600);
+//
+//        window.setScene(scene1);
+//        window.setTitle("title here");
+//        window.show();
+//        //switch scene done===
+
+        //2. alert window===
         window = primaryStage;
 
         Label label1 = new Label("this is the first scene.");
-        Button button1 = new Button("go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        Label label2 = new Label("this scene bad.");
-        Button button2 = new Button("go back to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
+        Button button1 = new Button("show alert");
+        button1.setOnAction(e -> AlertBox.display("a title", "a message"));
 
         //layout 1 - children are laid out in vertical column
         VBox layout1 = new VBox(20);
         layout1.getChildren().addAll(label1,button1);
         scene1 = new Scene(layout1, 200,200);
 
-        //layout 2 - laid out in horizontal
-        HBox layout2 = new HBox();
-        layout2.getChildren().addAll(button2, label2);
-        scene2 = new Scene(layout2, 600, 600);
 
         window.setScene(scene1);
         window.setTitle("title here");
         window.show();
-        //switch scene done===
+        //allert window===
     }
 
 
