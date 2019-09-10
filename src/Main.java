93a1339533fc3215e584b92f74a71f645cfd8b14
Ken <kensong96@gmail.com>
2019-09-10 12:19:52 -0400

    10.extract input and validation

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,21 @@ import javafx.stage.Stage;
 public class Main extends Application{
 
     Stage window;
-    Scene scene1, scene2;
-    //Button button;
+    //Scene scene1, scene2;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //primaryStage.setTitle("Hello World");
+
+        //0.template
+//        window = primaryStage;
+//        VBox vBox = new VBox();
+//        Scene scene = new Scene(vBox);
+//
+//        window.setScene(scene);
+//        window.setTitle("title here");
+//        window.show();
 
 //        //1. switch scene===
 //        window = primaryStage;
@@ -60,7 +68,6 @@ public class Main extends Application{
 //        layout1.getChildren().addAll(label1,button1);
 //        scene1 = new Scene(layout1, 200,200);
 //
-//
 //        window.setScene(scene1);
 //        window.setTitle("title here");
 //        window.show();
@@ -81,10 +88,10 @@ public class Main extends Application{
 //        layout1.getChildren().addAll(label1,button1);
 //        scene1 = new Scene(layout1, 200,200);
 //
-//
 //        window.setScene(scene1);
 //        window.setTitle("title here");
 //        window.show();
+
 
         //4.proper close
 //        window = primaryStage;
@@ -102,7 +109,6 @@ public class Main extends Application{
 //        VBox layout1 = new VBox(20);
 //        layout1.getChildren().addAll(label1,button1);
 //        scene1 = new Scene(layout1, 200,200);
-//
 //
 //        window.setScene(scene1);
 //        window.setTitle("title here");
@@ -138,39 +144,67 @@ public class Main extends Application{
 //        window.show();
 
         //6. gridpane
-        window = primaryStage;
-        window.setTitle("gridpane");
+//        window = primaryStage;
+//        window.setTitle("gridpane");
+//
+//        GridPane grid = new GridPane();
+//        grid.setPadding(new Insets(10,10,10,10));
+//        grid.setVgap(8);
+//        grid.setHgap(10);
+//
+//        //name label
+//        Label nameLabel = new Label("Username: ");
+//        GridPane.setConstraints(nameLabel, 0,0);
+//
+//        //name input
+//        TextField nameinput = new TextField("Ken");
+//        GridPane.setConstraints(nameinput,1,0);
+//
+//        //password label
+//        Label passwordlabel = new Label("Password: ");
+//        GridPane.setConstraints(passwordlabel, 0,1);
+//
+//        //name input
+//        TextField psdinput = new TextField();
+//        psdinput.setPromptText("password");
+//        GridPane.setConstraints(psdinput,1,1);
+//
+//        Button loginButton = new Button("log in");
+//        GridPane.setConstraints(loginButton,1,2);
+//
+//        grid.getChildren().addAll(nameLabel,nameinput,passwordlabel,psdinput,loginButton);
+//
+//        Scene scene = new Scene(grid,400,300);
+//        window.setScene(scene);
+//        window.show();
 
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10,10,10,10));
-        grid.setVgap(8);
-        grid.setHgap(10);
+        //7.extract and validate info
+        //use .getText to get the text
+//        window = primaryStage;
+//        VBox vBox = new VBox();
+//        TextField inputF = new TextField();
+//        Label result = new Label("result goes here");
+//        Button firm = new Button("confirm");
+//        firm.setOnAction(e->{
+//            result.setText(inputF.getText());
+//            isInt(inputF,inputF.getText());
+//        });
+//        vBox.getChildren().addAll(inputF,result,firm);
+//        Scene scene = new Scene(vBox,300,200);
+//
+//        window.setScene(scene);
+//        window.setTitle("title here");
+//        window.show();
 
-        //name label
-        Label nameLabel = new Label("Username: ");
-        GridPane.setConstraints(nameLabel, 0,0);
 
-        //name input
-        TextField nameinput = new TextField("Ken");
-        GridPane.setConstraints(nameinput,1,0);
-
-        //password label
-        Label passwordlabel = new Label("Password: ");
-        GridPane.setConstraints(passwordlabel, 0,1);
-
-        //name input
-        TextField psdinput = new TextField();
-        psdinput.setPromptText("password");
-        GridPane.setConstraints(psdinput,1,1);
-
-        Button loginButton = new Button("log in");
-        GridPane.setConstraints(loginButton,1,2);
-
-        grid.getChildren().addAll(nameLabel,nameinput,passwordlabel,psdinput,loginButton);
-
-        Scene scene = new Scene(grid,400,300);
-        window.setScene(scene);
-        window.show();
+        //8.checkbox
+//        window = primaryStage;
+//        VBox vBox = new VBox();
+//        Scene scene = new Scene(vBox);
+//
+//        window.setScene(scene);
+//        window.setTitle("title here");
+//        window.show();
 
     }
 
@@ -180,6 +214,18 @@ public class Main extends Application{
 //        if(result) {
 //            System.out.println("file saved");
 //            window.close();
+//        }
+//    }
+
+    //7.input validation
+//    private boolean isInt(TextField tf,String message){
+//        try{
+//            int age = Integer.parseInt(tf.getText());
+//            System.out.println("User is: " + age);
+//            return true;
+//        } catch(NumberFormatException e){
+//            System.out.println("error: " + message + " is not a number");
+//            return false;
 //        }
 //    }
 
